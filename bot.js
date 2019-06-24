d4860438403a07eff4e26314992f0eaab66c84f0
Melchior Kannengießer <melchior.kannengiesser@gmx.de>
2019-06-24 22:45:56 +0200

    add invite

--- bot.js
+++ bot.js
@@ -10,4 +10,10 @@ bot.on('ready', async () => {
   bot.user.setPresence({game: {name: 'ready to rock'}})
   .then(console.log)
   .catch(console.error);
+  try {
+    let link = await bot.generateInvite(["ADMINISTRATOR"]);
+    console.log(link);
+  } catch(e) {
+    console.log(e.stack);
+  }
 });
