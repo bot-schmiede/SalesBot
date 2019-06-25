cb715daf82ad2dc19a1e8eb31a7bcceb4904beaf
Niclas Scheel <scheel.niclas@gmail.com>
2019-06-25 22:07:46 +0200

    <commit message>

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const mongoose = require('mongoose');
 
+var fs = require("fs");
+
 const PREFIX = "$";
 
 const SaleController = require('./controllers/sales');
@@ -10,7 +12,7 @@ let bot = new Discord.Client();
 bot.login(process.env.TOKEN);
 
 bot.on('ready', async () => {
-  bot.user.setPresence({game: {name: 'ready to rock'}})
+  bot.user.setPresence({game: {name: 'type $help to start'}})
   .then(console.log)
   .catch(console.error);
   try {
@@ -40,18 +42,43 @@ mongoose.connect(
 mongoose.Promise = global.Promise;
 
 
-
-
 //Execute Commands
-bot.on('message', message => {
-  if(message.author.equals(bot.user)) return;
-  if(!message.content.startsWith(PREFIX)) return;
+bot.on("message",function(message) {
+  if (message.author.equals(bot.user)) return;
+  console.log(message.content);// gives you a output , what is writen in the Chat
+  if(!message.content.startsWith(PREFIX)) return;//return if there is no Prefix
+  var args = message.content.substring(PREFIX.length).split(" ");
 
-  msgArray = message.content.substring(PREFIX.length).split(' ');
-  let cmd = msgArray[0].toLowerCase();
-
-  switch(cmd) {
-    case 'foo':
+  //all Commands
+  switch (args[0].toLowerCase()) {
+    case "test"://just for testing purpose(sends message)
+      message.channel.send("test done");
+      break;
+    case "testembed"://just for testing purpose(sends embed)
+      var embedtest = new Discord.RichEmbed()
+      .setColor(0xcc00cc)
+      .addField("by: " + message.member,"command: " + message.content)
+      message.channel.send(embedtest)
+      break;
+      //here the sales should be displayed
+    case "sales":
+      fs.readFile("sales.txt", "utf-8", (err, data) => {
+        var Salesdata = data
+        var Salesembed = new Discord.RichEmbed()
+        .setTitle("Sales")
+        .addField("Sales we currently have saved",data)
+        message.channel.send(Salesembed);
+      });
+      SaleController.get_all(message);
+      break;
+    case "addsale"://ads a Sale to the List
+      const args2 = message.content.slice(PREFIX.length).trim().split(/ +/g);
+      var Addsale = args[1] // here schould be the link to the sale
+      fs.writeFile("sales.txt", Addsale+ "\r\n",{ flag: 'a+' }, (err) => {// here you should the Command to save it in the database
+        if (err) console.log(err);
+      });
+      console.log(Addsale +"saved");
+      message.channel.send("your sale got saved")
       //dumy data
       let saleData = {
         link: "http://foo.bar.baz",
@@ -61,11 +88,18 @@ bot.on('message', message => {
       };
       SaleController.add_sale(saleData, message);
       break;
-    case 'bar':
-    SaleController.get_all(message);
-    break;
-    default:
-      message.channel.send('soon tm');
+    case "help"://help Command
+      var Helpembed = new Discord.RichEmbed()
+      .setColor(0x00008B)
+      .setTitle("SaleBot Commands:")
+      .addField("Prefix : $")
+      .addField("$help","gives you this list")
+      .addField("$sales","shows you all the Sales we currently have saved in our data")
+      .addField("$addsale [Salelink]","you can add a Sale to the Saleslist")
+      message.channel.send(Helpembed);
       break;
-  }
-});
+    default:
+      message.channel.send('nope! go away!');
+      break;
+  };//switch Ding
+});//message fuction
