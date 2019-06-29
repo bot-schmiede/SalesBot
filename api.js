ff5ed4f4122ff81308373c759a5cef1996470f59
Melchior Kannengießer <melchior.kannengiesser@gmx.de>
2019-06-29 03:31:22 +0200

    add error handling

--- api.js
+++ api.js
@@ -8,4 +8,18 @@ api.use(morgan('dev'));
 
 api.use('/sales', saleRoutes);
 
+api.use((req, res, next) => {
+  const error = new Error('not found');
+  error.status = 404;
+  next(error);
+});
+
+api.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
 module.exports = api;
