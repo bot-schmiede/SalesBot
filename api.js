71ceeee63f5ac222ea0da0104df8591c4685922e
Melchior Kannengießer <melchior.kannengiesser@gmx.de>
2019-06-29 03:14:12 +0200

    add logging to api

--- api.js
+++ api.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const api = express();
+const morgan = require('morgan');
 
 const saleRoutes = require('./api/routes/sales');
+
+api.use(morgan('dev'));
 
 api.use('/sales', saleRoutes);
 
